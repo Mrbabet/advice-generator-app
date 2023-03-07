3ee6ec30c5f1245f1e53979b2f617f8f9bea6c45
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2023-03-07 10:21:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ advice.textContent = "";
 const fetchData = async function () {
   const response = await fetch(url);
   const data = await response.json();
+  if (!response.ok) {
+    switch (response.status) {
+      case 400:
+        console.log("error 400");
+        break;
+      case 401:
+        console.log("error 401");
+        break;
+      case 404:
+        console.log("error 404");
+        break;
+      case 500:
+        console.log("error 500");
+        break;
+    }
+  }
   return data;
 };
 
 const loadData = async function () {
-  const value = await fetchData().catch((err) => handle(err));
+  const value = await fetchData();
   advice.textContent = value.slip.advice;
 };
 loadData();
